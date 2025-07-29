5dd1d6cf0979917d938529d59cc796ce3c173425
Sara Wendland <info@rosebudsara.com>
2025-07-28 21:50:56 -0500

    fix server for deployment

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
 // Serve React build static files in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static("./client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile("client/build/index.html", { root: "." });
   });
 }
 
